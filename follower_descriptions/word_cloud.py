b7d5d28649fdcc8b9a8dfb6edcbc61a2b83fbea5
Steve <lewis.brown@capgemini.com>
2016-05-08 17:50:49 +0100

    Word cloud from frequency tuples

--- follower_descriptions/word_cloud.py
+++ follower_descriptions/word_cloud.py
@@ -16,21 +16,15 @@ import matplotlib.pyplot as plt
 import twitter.settings
 
 def GenerateWordCloud():
-    d = path.dirname(__file__)
 
     # Read the whole text.
-    text = open(path.join(d, 'word_test.txt')).read()
+    #d = path.dirname(__file__)
+    #text = open(path.join(d, 'word_test.txt')).read()
+    #wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(text)
 
-    # Generate a word cloud image
-    #wordcloud = WordCloud().generate(text)
+    frequencies = [('one', 1), ('two', 0.5)]
+    wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate_from_frequencies(frequencies)
 
-    # Display the generated image:
-    # the matplotlib way:
-    #plt.imshow(wordcloud)
-    #plt.axis("off")
-
-    # take relative word frequencies into account, lower max_font_size
-    wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(text)
     plt.figure()
     plt.imshow(wordcloud)
     plt.axis("off")
